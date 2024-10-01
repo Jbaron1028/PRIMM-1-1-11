7371bb5047a4aae3881e969fff414218f3ff7930
Jbaron1028 <yontic2007@gmail.com>
2024-10-01 15:40:39 -0600

    Update primm_11.py by changing the variable types from str to int

--- primm_11.py
+++ primm_11.py
@@ -6,11 +6,11 @@ Name - Date
 
 def main():
   
-    num1: int = input("Enter a number: ")
-    num2: int = input("Enter another number: ")
-    total: int = num1+num2
+    num1: int = int(input("Enter a number: ")) #creating a new variable and asking the user for the input and changing the variable type to int from str
+    num2: int = int(input("Enter another number: ")) #creating a new variable and asking the user for the input and changing the variable type to int from str
+    total: int = num1+num2 #creating a new variable and adding up the two inputs
 
-    print(f"{num1} + {num2} = {total}")
+    print(f"{num1} + {num2} = {total}") #printing out the total
 
 if __name__ == "__main__":
   main()
